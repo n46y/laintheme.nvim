247777420066a9ad069ac1810c5b025d3d28a0fa
n46y <nagy.gusztav.work@gmail.com>
2024-10-31 17:00:02 +0100

    maybe

---
+++ lua/lackluster/color.lua
@@ -0,0 +1,22 @@
+---@class LacklusterColor
+return {
+    lack = "#b43453",
+    luster = "#deeeed",
+    orange = "#d2738a",
+    yellow = "#e9b6c2",
+    green = "#789978",
+    blue = "#7788AA",
+    red = "#92082a",
+    none = "none",
+
+    black = "#000000",
+    gray1 = "#080808",
+    gray2 = "#191919",
+    gray3 = "#2a2a2a",
+    gray4 = "#444444",
+    gray5 = "#555555",
+    gray6 = "#7a7a7a",
+    gray7 = "#aaaaaa",
+    gray8 = "#cccccc",
+    gray9 = "#DDDDDD",
+}
